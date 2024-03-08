89f92efb474892123dde9527316607e619f82c72
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2024-03-08 11:24:16 -0300

    req 6 done

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,16 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority_queue = PriorityQueue()
+    assert priority_queue.is_priority({"qtd_linhas": 4}) is True
+    assert priority_queue.is_priority({"qtd_linhas": 9}) is False
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        priority_queue.search(-1)
+    priority_queue.enqueue({"qtd_linhas": 9})
+    priority_queue.enqueue({"qtd_linhas": 4})
+    priority_queue.enqueue({"qtd_linhas": 2})
+    assert len(priority_queue) == 3
+    assert priority_queue.dequeue() == {"qtd_linhas": 4}
+    assert priority_queue.search(0) == {"qtd_linhas": 2}
