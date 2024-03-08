b170a4d54b5ce3dcbcdf3063195d77793c9f4ed2
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2024-03-07 23:00:54 -0300

    req 2 done

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido")
+    try:
+        with open(path_file, "r") as file:
+            result_list = []
+            content = file.read()
+            content = content.split("\n")
+            for line in content:
+                result_list.append(line)
+        return result_list
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
