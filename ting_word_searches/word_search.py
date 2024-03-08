92dfe1731ae0b8d42b3c0606158be633cf250461
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2024-03-08 16:34:10 -0300

    req 7 done

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,37 @@
+"""from ting_file_management.file_process import process
+from ting_file_management.queue import Queue
+"""
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result_list = []
+    for i in range(len(instance.queue)):
+        search = instance.search(i)
+        ocorrencias = [
+            {"linha": idc + 1}
+            for idc, line in enumerate(search["linhas_do_arquivo"])
+            if word.lower() in line.lower()
+        ]
+        if ocorrencias:
+            result_list.append(
+                {
+                    "palavra": word,
+                    "arquivo": search["nome_do_arquivo"],
+                    "ocorrencias": ocorrencias,
+                }
+            )
+        else:
+            result_list = []
+
+    return result_list
 
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+
+
+"""project = Queue()
+process("statics/nome_pedro.txt", project)
+word = exists_word("pedro", project)
+print(word)
+"""
